67a92aeac59a97de610f51c0a2b7ef248e508547
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-11 01:13:15 -0300

    nested routes and some refactoring

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,22 @@
+import Menu from "components/Menu";
+import PaginaPadrao from "components/PaginaPadrao";
 import Cardapio from "pages/Cardapio";
 import Inicio from "pages/Inicio";
 import { Route, Routes } from "react-router";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function AppRouter() {
-    return(
-        <Router>
-            <Routes>
-                <Route path='/' element={<Inicio/>}/>
-                <Route path='/cardapio' element={<Cardapio/>}/>
-            </Routes>
-        </Router>
+    return (
+        <main>
+            <Router>
+                <Menu/>
+                <Routes>
+                    <Route path="/" element={<PaginaPadrao/>}>
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </main>
     );
 }
